0db6fd654ae8c651ba46f6920b6a49a74e455684
Jason Lee <jason0409lee@gmail.com>
2024-05-04 21:33:52 -0700

    Some changes made for a feedback form

--- background.js
+++ background.js
@@ -43,7 +43,7 @@
     //For Feedback Form
     chrome.runtime.onInstalled.addListener(details => {
         if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-            chrome.runtime.setUninstallURL("https://forms.gle/QJT3UUBPwZBkajXf9");
+            chrome.runtime.setUninstallURL("https://forms.gle/SA7shHbtMaFYczxv7");
         }
       });
     })()
